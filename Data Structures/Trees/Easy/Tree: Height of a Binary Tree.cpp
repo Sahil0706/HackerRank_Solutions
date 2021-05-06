03baa17b7367f72951217d4c59bb1534a81835d6
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-05-06 10:25:52 +0530

    Create Tree: Height of a Binary Tree.cpp

---
+++ Data Structures/Trees/Easy/Tree: Height of a Binary Tree.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        return h(root)==0 ? 0 : h(root)-1;
+    }
+    int h(Node* root) {
+        if(root == NULL)
+            return 0;
+            
+        int lDepth = h(root->left);
+        int rDepth = h(root->right);
+        
+        return 1 + max(lDepth,rDepth);
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
