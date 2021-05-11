a9a4c7b8820e1da3900112067d39e052e33ee8b6
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-05-11 18:52:02 +0530

    Create Balanced Brackets.cpp

---
+++ Data Structures/Stacks/Medium/Balanced Brackets.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string isBalanced(string s) {
+    stack<char>st;
+    int n = s.length();
+    for(int i=0; i<n; i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            st.push(s[i]);
+        else{
+            if(!st.empty()){
+                if(s[i] == ')' && st.top() == '(')
+                    st.pop();
+                else if(s[i] == '}' && st.top() == '{')
+                    st.pop();
+                else if(s[i] == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    return "NO";   
+            }
+            else {
+                return "NO";
+            }
+        }
+    }
+    if(st.empty())
+        return "YES";
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
