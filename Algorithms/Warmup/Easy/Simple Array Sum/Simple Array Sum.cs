032d0122ef6141f0089e3462ed7d9826b7d23f5a
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-05-14 11:11:26 +0530

    Update Simple Array Sum.cs

--- Algorithms/Warmup/Easy/Simple Array Sum/Simple Array Sum.cs
+++ Algorithms/Warmup/Easy/Simple Array Sum/Simple Array Sum.cs
@@ -1,106 +1,53 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
-vector<int> compareTriplets(vector<int> a, vector<int> b) {
-
-}
-
-int main()
+class Result
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
 
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
 
-    vector<string> a_temp = split(rtrim(a_temp_temp));
-
-    vector<int> a(3);
-
-    for (int i = 0; i < 3; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
+    public static int simpleArraySum(List<int> ar)
+    {
+        int s = 0;
+        foreach(int i in ar)
+            s+=i;
+        
+        return s;
     }
 
-    string b_temp_temp;
-    getline(cin, b_temp_temp);
-
-    vector<string> b_temp = split(rtrim(b_temp_temp));
-
-    vector<int> b(3);
-
-    for (int i = 0; i < 3; i++) {
-        int b_item = stoi(b_temp[i]);
-
-        b[i] = b_item;
-    }
-
-    vector<int> result = compareTriplets(a, b);
-
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-    return s;
-}
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
 
-string rtrim(const string &str) {
-    string s(str);
+        int result = Result.simpleArraySum(ar);
 
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
+        textWriter.WriteLine(result);
 
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
+        textWriter.Flush();
+        textWriter.Close();
     }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
