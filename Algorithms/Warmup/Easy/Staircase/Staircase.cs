c6bb0613611f8aab75cb61115264054f2d59c0e5
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-05-15 17:57:16 +0530

    Create Staircase.cs

---
+++ Algorithms/Warmup/Easy/Staircase/Staircase.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for(int i=1;i<=n;i++){
+            string s = "";
+            int k = n-i;
+            while(k-- != 0){
+                s+=" ";
+            }
+            k=i;
+            while(k-- != 0){
+                s+="#";
+            }
+            Console.WriteLine(s);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
