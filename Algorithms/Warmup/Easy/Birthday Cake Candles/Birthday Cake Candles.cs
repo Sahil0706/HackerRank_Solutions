746b9989a433e2dfb3cb7278956e3aa68e567c1b
Sahil0706 <65964632+Sahil0706@users.noreply.github.com>
2021-05-18 21:31:38 +0530

    Create Birthday Cake Candles.cs

---
+++ Algorithms/Warmup/Easy/Birthday Cake Candles/Birthday Cake Candles.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int maxele = candles[0];
+        for(int i=0; i<candles.Count(); i++){
+            if(maxele < candles[i]){
+                maxele = candles[i];
+            }
+        }
+        int c = 0;
+        for(int i=0; i<candles.Count(); i++){
+            if(maxele == candles[i]){
+                c++;
+            }
+        }
+        return c;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
