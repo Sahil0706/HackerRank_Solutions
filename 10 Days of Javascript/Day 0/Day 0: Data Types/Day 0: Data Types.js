dd1956cba1caa64a2ff6e8c761058742a4eaf028
Sahil <sahil20000706@gmail.com>
2021-11-01 18:50:29 +0530

    Update Day 0: Data Types.js

--- 10 Days of Javascript/Day 0/Day 0: Data Types/Day 0: Data Types.js
+++ 10 Days of Javascript/Day 0/Day 0: Data Types/Day 0: Data Types.js
@@ -1,26 +1,4 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /**
 *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
@@ -36,29 +14,21 @@ function readLine() {
 **/
 function performOperation(secondInteger, secondDecimal, secondString) {
     // Declare a variable named 'firstInteger' and initialize with integer value 4.
-    const firstInteger = 4;
+    var firstInteger = 4;
     
     // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
-    const firstDecimal = 4.0;
+    var firstDecimal = 4.0;
     
     // Declare a variable named 'firstString' and initialize with the string "HackerRank".
     const firstString = 'HackerRank ';
     
     // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
-    
+    console.log(Number(firstInteger)+Number(secondInteger));
     
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    
+    console.log(Number(firstDecimal) + Number(secondDecimal));
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    
+    console.log(firstString+secondString);
 }
 
-
-function main() {
-    const secondInteger = readLine();
-    const secondDecimal = readLine();
-    const secondString = readLine();
-    
-    performOperation(secondInteger, secondDecimal, secondString);
-}
