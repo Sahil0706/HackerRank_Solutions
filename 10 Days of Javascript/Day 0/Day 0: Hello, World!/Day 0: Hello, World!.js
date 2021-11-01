16fa437628755085f8ac7833d2c18f7fa01204f7
Sahil <sahil20000706@gmail.com>
2021-11-01 18:51:15 +0530

    Update Day 0: Hello, World!.js

--- 10 Days of Javascript/Day 0/Day 0: Hello, World!/Day 0: Hello, World!.js
+++ 10 Days of Javascript/Day 0/Day 0: Hello, World!/Day 0: Hello, World!.js
@@ -1,26 +1,4 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
@@ -34,12 +12,7 @@ function greeting(parameterVariable) {
     console.log('Hello, World!');
 
     // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
     
 }
 
-
-function main() {
-    const parameterVariable = readLine();
-    
-    greeting(parameterVariable);
-}
