eed7d2bfcbbea82cbc011b3c027f164e9216a0d3
Sahil <sahil20000706@gmail.com>
2021-11-01 18:54:12 +0530

    Create Day 1: Let and Const.js

---
+++ 10 Days of Javascript/Day 1/Day 1: Let and Const/Day 1: Let and Const.js
@@ -0,0 +1,10 @@
+
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = parseFloat(readLine());
+    // Print the area of the circle:
+    var helper = Math.PI * r;
+    console.log(helper*r);
+    // Print the perimeter of the circle:
+    console.log(2*helper);
