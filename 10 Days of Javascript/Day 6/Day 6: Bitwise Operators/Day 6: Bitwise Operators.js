0a0df8ada1491b3848992407af044bbe4683dcfe
Sahil <sahil20000706@gmail.com>
2021-11-04 18:47:05 +0530

    Create Day 6: Bitwise Operators.js

---
+++ 10 Days of Javascript/Day 6/Day 6: Bitwise Operators/Day 6: Bitwise Operators.js
@@ -0,0 +1,17 @@
+
+function getMaxLessThanK(n, k) {
+    let maximum = 0;
+    let current = -1;
+    for (let i = 1; i < k; i++) 
+    {
+        for (let j = i + 1; j <= n; j++) 
+        {
+            current = i & j;
+            if (current < k && current > maximum) 
+            {
+                maximum = current;
+            }
+        }
+    }
+    return maximum;
+}
